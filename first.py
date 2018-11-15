cc587a7e3c8a25a3fc51e272330cf5f0a55d74e5
unknown <ehwjrdufvnd@naver.com>
2018-11-15 19:35:20 +0900

    first

---
+++ first.py
@@ -0,0 +1,18 @@
+#A
+s = input("얼마를 가지고 계세요?")
+print("입력한 값: %s" %s)
+#B
+a: str = "가나다라마\n바사아"
+print(a)
+#C
+d = input("이름: \n")
+print(d*10)
+#D
+age = int(input("나이를 입력하세요:"))
+name = input("이름을 입력하세요:")
+age=str(age)
+print("나의 이름은 %s이고 %s 세입니다" %(name,age))
+
+
+
+
